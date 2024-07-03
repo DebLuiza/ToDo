361337c851fe05cbbe1b0ab177d3a87ce423ebc9
DebLuizaPuc <debora.luiza@rethink.dev>
2024-07-03 11:32:12 -0300

    First Commit

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
+  const [task, setTask] = useState('');
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    if (task.trim().length > 0) {
+      setTaskItems([...taskItems, task]);
+      setTask(null);
+    }
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Today's Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+        <View style={styles.items}>
+          {/* Tasks list */}
+          {
+            taskItems.map((el, index) => (
+              <Pressable key={index} onPress={() => completeTask(index)}>
+                <Task text={el} />
+              </Pressable>
+            ))
+          }
+        </View>
+      </View>
+      {/* Task input */}
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? "padding" : "height"} style={styles.writeTasksWrapper}>
+        <TextInput 
+          style={styles.input} 
+          placeholder={'Write a task'} 
+          value={task} 
+          onChangeText={text => setTask(text)} 
+        />
+        <Pressable onPress={handleAddTask}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </Pressable>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -13,8 +57,48 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E8EAED',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeTasksWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     alignItems: 'center',
+  },
+  input: {
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    width: 250,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#c0c0c0',
+    borderWidth: 1,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#c0c0c0',
+    borderWidth: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addText: {
+    fontSize: 24,
+    color: '#000',
   },
 });
